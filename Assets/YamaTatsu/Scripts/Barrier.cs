c1e287fac21459c1e11cb69a8432971d414d1896
YamaTatsu8 <tatsu8fate@icloud.com>
2018-07-30 12:23:15 +0900

    Playerの設定

---
+++ Assets/YamaTatsu/Scripts/Barrier.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour {
+
+    //コントローラー
+    GameController controller;
+
+    //バリア
+    [SerializeField]
+    private GameObject _barrier;
+
+    private GameObject _object;
+
+    //バるあフラグ
+    private bool _flag = false;
+
+    //生成してるかどうか
+    private bool _reFlag = false;
+
+    //リミット
+    [SerializeField]
+    private float _limitTime = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+
+        controller = GameController.Instance;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        controller.ControllerUpdate();
+
+
+        if (controller.ButtonDown(Button.L1))
+        {
+            _flag = !_flag;
+        }
+       
+
+        if (_flag == true && _reFlag == false)
+        {
+            _object = GameObject.Instantiate(_barrier);
+            _reFlag = true;
+        }
+        else if(_flag == false)
+        {
+            Destroy(_object);
+            _reFlag = false;
+        }
+
+        if(_reFlag == true)
+        {
+            _object.transform.position = transform.position;
+        }
+
+    }
+}
