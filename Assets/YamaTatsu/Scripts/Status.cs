caf7c1e96b90fd7086224b521a197fd53f2a5cfc
YamaTatsu8 <tatsu8fate@icloud.com>
2018-07-30 12:23:15 +0900

    Playerの設定

---
+++ Assets/YamaTatsu/Scripts/Status.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour {
+
+    //　HP
+    [SerializeField]
+    private float _HP = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        // HPが0以下の場合破壊
+        if(_HP < 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+	}
+
+    public bool getDestroy()
+    {
+        return true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            //_HP -=
+        }
+    }
+
+}
