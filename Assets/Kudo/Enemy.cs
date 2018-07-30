764149b844e586132d00564c35482769175a51f5
Kudo <soramoti0505@gmail.com>
2018-07-30 12:20:59 +0900

    Ememyの移動、攻撃が簡易的に完成

---
+++ Assets/Kudo/Enemy.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+    public enum State
+    {
+        ShortAttack,    // 近距離時
+        DistantAttack   // 遠距離時
+    }
+
+    public GameObject _target;
+    public float _speed;
+
+    public GameObject _gun;
+    public GameObject _sword;
+    public GameObject _bullet;
+
+    private State _state;
+    private int _changeCount = 0;
+    private int _bulletCount = 0;
+    private float _bulletPower = 500f;
+
+
+    void Start () {
+        _state = State.DistantAttack;
+        Debug.Log(_state);
+    }
+
+    void Update () {
+
+        // 3秒ごとに遠距離か近距離か決める
+        _changeCount++;
+        if(_changeCount == 180)
+        {
+            SetState();
+            _changeCount = 0;
+            Debug.Log(_state);
+        }
+
+
+        switch (_state)
+        {
+            case State.ShortAttack:
+                _sword.SetActive(true);
+                _gun.SetActive(false);
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target.transform.position - transform.position), 0.3f);
+                transform.position += transform.forward * _speed;
+                break;
+            case State.DistantAttack:
+                _sword.SetActive(false);
+                _gun.SetActive(true);
+
+                Ray ray = new Ray(transform.position, _target.transform.position);
+                RaycastHit hit = new RaycastHit();
+
+                if (Physics.Raycast(ray.origin, ray.direction, out hit,10))
+                {
+                    if (hit.collider.gameObject.CompareTag("Player"))
+                    {
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target.transform.position - transform.position), 0.3f);
+                        transform.position -= transform.forward * _speed;
+                    }
+
+                    //if (hit.collider.gameObject.CompareTag("Wall"))
+                    //{
+                    //}
+
+                }
+
+                Debug.DrawRay(ray.origin + ray.direction, ray.direction, Color.yellow);
+
+                //_bulletCount++;
+                //if (_bulletCount > 30)
+                //{
+                //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target.transform.position - transform.position), 0.3f);
+                //    var bulletInstance = Instantiate(_bullet, _gun.transform.position, _gun.transform.rotation) as GameObject;
+                //    bulletInstance.GetComponent<Rigidbody>().AddForce(bulletInstance.transform.forward * _bulletPower);
+                //    Destroy(bulletInstance, 5f);
+                //    _bulletCount = 0;
+                //}
+                break;
+            default:
+                break;
+
+        }
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            switch (_state)
+            {
+                case State.ShortAttack:
+                    // ダメージ判定（プレイヤーへ
+                    break;
+                case State.DistantAttack:
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target.transform.position - transform.position), 0.3f);
+                    transform.position -= transform.forward * _speed;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    // ステートの切り替え（ランダムで切替
+    void SetState()
+    {
+        int num = Random.Range(0, 2);
+
+        switch (num)
+        {
+            case 0:
+                _state = State.ShortAttack;
+                break;
+            case 1:
+                _state = State.DistantAttack;
+                break;
+        }
+
+    }
+}
