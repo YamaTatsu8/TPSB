5702b72737f493c3c529de1f23b353ecc8db2cf6
Masashi <masashi77823@gmail.com>
2018-07-31 12:05:24 +0900

    Revert "Revert "弾発射するやつ追加""
    This reverts commit a20a10a15c54a74124f272847a12cc1a31a5c246.

---
+++ Assets/zNiki/Scripts/SuperTester.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperTester : MonoBehaviour
+{
+    GameController con;
+
+    // Use this for initialization
+    void Start ()
+    {
+        con = GameController.Instance;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // このUpdateは必須
+        con.ControllerUpdate();
+
+        // このifでコントローラが刺さってるか判定する
+        if (con.GetConnectFlag())
+        {
+            if(con.TriggerDown(Trigger.Left))
+            {
+                this.gameObject.GetComponent<RayCastShoot>().Shot();
+            }
+        }
+        else
+        {
+            Debug.Log("つっかえ！");
+        }
+    }
+}
