0e1f5d3c37b71fd0cc2a9abe55f6684e34ed467c
Masashi <masashi77823@gmail.com>
2018-11-02 10:09:29 +0900

    テスト ダメージ付与追加

--- Assets/zNiki/Scripts/Weapon/BulletController.cs
+++ Assets/zNiki/Scripts/Weapon/BulletController.cs
@@ -20,11 +20,11 @@ public class BulletController : MonoBehaviour
         });
     }
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    if (collision.gameObject.tag == "Player")
-    //    {
-    //        collision.gameObject.GetComponent<Status>()
-    //    }
-    //}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Status>().HitDamage(_bulletDamage);
+        }
+    }
 }
