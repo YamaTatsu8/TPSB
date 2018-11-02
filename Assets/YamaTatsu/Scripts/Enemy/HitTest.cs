da6eed2ada5ee7de6d23ef6d466a9336fbb18174
YamaTatsu8 <tatsu8fate@icloud.com>
2018-11-02 11:08:59 +0900

    ミサイルの変更

---
+++ Assets/YamaTatsu/Scripts/Enemy/HitTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitTest : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //ヒット判定
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            Debug.Log("hit");
+        }
+    }
+
+}
