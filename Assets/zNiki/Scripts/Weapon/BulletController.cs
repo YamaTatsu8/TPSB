7a8d6779fdda94758cbf5a7ef512e5fdeeecf376
Masashi <masashi77823@gmail.com>
2018-11-02 10:29:59 +0900

    no message

--- Assets/zNiki/Scripts/Weapon/BulletController.cs
+++ Assets/zNiki/Scripts/Weapon/BulletController.cs
@@ -20,6 +20,7 @@ public class BulletController : MonoBehaviour
         });
     }
 
+<<<<<<< 0e1f5d3c37b71fd0cc2a9abe55f6684e34ed467c
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -27,4 +28,13 @@ public class BulletController : MonoBehaviour
             collision.gameObject.GetComponent<Status>().HitDamage(_bulletDamage);
         }
     }
+=======
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        collision.gameObject.GetComponent<Status>()
+    //    }
+    //}
+>>>>>>> バグ修正
 }
