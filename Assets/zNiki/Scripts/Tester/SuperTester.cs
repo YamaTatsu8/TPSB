393044f2bb36b37ddd93bcfeeb282463ac659425
Masashi <masashi77823@gmail.com>
2018-11-14 09:25:01 +0900

    仮追加

--- Assets/zNiki/Scripts/Tester/SuperTester.cs
+++ Assets/zNiki/Scripts/Tester/SuperTester.cs
@@ -16,57 +16,64 @@ public class SuperTester : MonoBehaviour
 
     [SerializeField]
     private GameObject _sub;
-    
+
+    private void Awake()
+    {
+        SoundManager.LoadBgm("bgm", "02_Trailer2");
+    }
+
     // Use this for initialization
     void Start ()
     {
-        con = GameController.Instance;        
+        con = GameController.Instance;
+
+        Debug.Log(SoundManager.PlayBgm("bgm"));
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-        // このUpdateは必須
-        con.ControllerUpdate();
+        //// このUpdateは必須
+        //con.ControllerUpdate();
 
-        // このifでコントローラが刺さってるか判定する
-        if (con.GetConnectFlag())
-        {
-            if (con.TriggerDown(Trigger.LEFT))
-            {
-                if (_isEquippedMain1)
-                {
-                    _main1.GetComponent<WeaponManager>().Attack();
-                }
-                else
-                {
-                    _main2.GetComponent<WeaponManager>().Attack();
-                }
-            }
-            if (con.TriggerDown(Trigger.RIGHT))
-            {
-                _sub.GetComponent<WeaponManager>().Attack();
-            }
-            if (con.ButtonDown(Button.X))
-            {
-                _isEquippedMain1 = !_isEquippedMain1;
-            }
-        }
-        else
-        {
-            Debug.Log(con.GetConnectFlag());
-        }
+        //// このifでコントローラが刺さってるか判定する
+        //if (con.GetConnectFlag())
+        //{
+        //    if (con.TriggerDown(Trigger.LEFT))
+        //    {
+        //        if (_isEquippedMain1)
+        //        {
+        //            _main1.GetComponent<WeaponManager>().Attack();
+        //        }
+        //        else
+        //        {
+        //            _main2.GetComponent<WeaponManager>().Attack();
+        //        }
+        //    }
+        //    if (con.TriggerDown(Trigger.RIGHT))
+        //    {
+        //        _sub.GetComponent<WeaponManager>().Attack();
+        //    }
+        //    if (con.ButtonDown(Button.X))
+        //    {
+        //        _isEquippedMain1 = !_isEquippedMain1;
+        //    }
+        //}
+        //else
+        //{
+        //    Debug.Log(con.GetConnectFlag());
+        //}
 
-        if (_isEquippedMain1)
-        {
-            _main1.SetActive(true);
-            _main2.SetActive(false);
-        }
-        else
-        {
-            _main1.SetActive(false);
-            _main2.SetActive(true);
-        }
+        //if (_isEquippedMain1)
+        //{
+        //    _main1.SetActive(true);
+        //    _main2.SetActive(false);
+        //}
+        //else
+        //{
+        //    _main1.SetActive(false);
+        //    _main2.SetActive(true);
+        //}
 
         //if (Input.GetMouseButton(0))
         //{
