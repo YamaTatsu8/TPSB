53ee3d388eeb368511d31d1b0bf48c41880c91eb
YamaTatsu8 <tatsu8fate@icloud.com>
2018-11-16 11:28:01 +0900

    no message

---
+++ Assets/YamaTatsu/Scripts/Enemy/EnemyStatus.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStatus : MonoBehaviour {
+
+    //　敵のMaxHP
+    [SerializeField]
+    private int maxHp = 3;
+    //　敵のHP
+    private int hp;
+    //　敵の攻撃力
+    [SerializeField]
+    private int attackPower = 1;
+    private Enemy enemy;
+    //　敵のHPバー処理スクリプト
+    private EnemyHP hpStatusUI;
+
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+        hp = maxHp;
+        hpStatusUI = GetComponentInChildren<EnemyHP>();
+    }
+
+    public void SetHp(int hp)
+    {
+        this.hp = hp;
+
+        //　HP表示用UIのアップデート
+        hpStatusUI.UpdateHPValue();
+
+        if (hp <= 0)
+        {
+            //　HP表示用UIを非表示にする
+            hpStatusUI.SetDisable();
+        }
+    }
+
+    public int GetHp()
+    {
+        return hp;
+    }
+
+    public int GetMaxHp()
+    {
+        return maxHp;
+    }
+}
