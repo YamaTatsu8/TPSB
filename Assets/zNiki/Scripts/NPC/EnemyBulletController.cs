06ff3e62c91f07012224bd521f857152af8e7937
Masashi <masashi77823@gmail.com>
2018-11-22 19:44:26 +0900

    敵AI追加したい

---
+++ Assets/zNiki/Scripts/NPC/EnemyBulletController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletController : MonoBehaviour
+{
+    // 弾のダメージ
+    [SerializeField]
+    private int _bulletDamage = 1;
+
+    // 消滅までの時間
+    [SerializeField]
+    private float _destroyTime = 3.0f;
+
+    public void DeleteBullet(GameObject bulletClone)
+    {
+        this.Delay(_destroyTime, () =>
+        {
+            Destroy(bulletClone);
+        });
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Target")
+        {
+            Destroy(this.gameObject);
+
+            collision.gameObject.GetComponent<Status>().hitDamage(_bulletDamage);
+        }
+        if (collision.gameObject.tag == "Ground")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
