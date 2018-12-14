78c0ff42c6fe48cfa22b98784b93e6a047506d54
Masashi <masashi77823@gmail.com>
2018-12-14 10:13:04 +0900

    AudioManager追加

---
+++ Assets/Scripts/Constants/AUDIO.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// オーディオ名を定数で管理するクラス
+/// </summary>
+public static class AUDIO
+{
+	  public const string BGM_BGM_MAOUDAMASHII_CYBER39 = "bgm_maoudamashii_cyber39";
+	  public const string BGM_CUSTMA = "custma";
+	  public const string BGM_TITLE = "title";
+	
+	  public const string SE_BLIP02 = "blip02";
+	  public const string SE_CANCEL5 = "cancel5";
+	  public const string SE_DECISION17 = "decision17";
+	  public const string SE_LAUNCHER5 = "launcher5";
+	  public const string SE_SELECT09 = "select09";
+	  public const string SE_SHOOT1 = "shoot1";
+	  public const string SE_SUBMACHINEGUN1 = "submachinegun1";
+}
