aa7e2156dfc166afc91fb6994acf9141eae0f0cf
YamaTatsu8 <tatsu8fate@icloud.com>
2019-01-15 14:14:04 +0900

    カスタマイズ画面の編集

---
+++ Assets/YamaTatsu/Scripts/System/CustomWeapon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomWeapon : MonoBehaviour {
+
+    //武器一覧
+    [SerializeField]
+    private GameObject[] _weapon;
+
+    // Use this for initialization
+    void Start () {
+
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+       
+	}
+
+    public void setWeapon(string name)
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            if(_weapon[i].name == name)
+            {
+                _weapon[i].SetActive(true);
+            }
+            else
+            {
+                _weapon[i].SetActive(false);
+            }
+        }
+    }
+
+}
